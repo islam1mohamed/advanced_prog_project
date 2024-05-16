6c4059355c95490a4145130731d9c1f1282aacff
Islam Mohamed <130303768+islam1mohamed@users.noreply.github.com>
2024-05-16 11:32:09 +0300

    Delete oop_project directory

--- oop_project/src/main/java/com/example/oop_project/Refundable.java
+++
@@ -1,5 +0,0 @@
-package com.example.oop_project;
-
-public interface Refundable {
-    public abstract void refund(int amount);
-}
--- oop_project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.oop_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.oop_project to javafx.fxml;
-    exports com.example.oop_project;
-}
